efc9515a3ef4bdcc59a44415c0be896e34a1949b
alikpid <koromislo.molodec@gmail.com>
2023-03-06 13:08:10 +0700

    Директивы v-if, v-show. Не работает v-else. ПРАКТ 3

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        inStock: true,
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
     }
 })
